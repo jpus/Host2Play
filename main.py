0b6f74aea05fd26495be9e052e897acfe2e13bdb
jpus <zengqcyxx@gmail.com>
2026-08-03 19:33:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=f9ba3248-9756-48ea-aabd-a5d8ecb940e9",
-    "https://host2play.gratis/server/renew?i=cf93abbb-7460-459b-8b60-5e46c9855a16",
+    "https://host2play.gratis/server/renew?i=083a877a-4530-42e1-9b59-7a2f11a9f150",
   
 ]
 
